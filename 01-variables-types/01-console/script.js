9674345dccb565655fc8cd511096ac20bcf6287e
Devendra <devekr12@gmail.com>
2024-09-03 00:35:26 +0530

    type conversion

---
+++ 01-variables-types/01-console/script.js
@@ -0,0 +1,39 @@
+// alert('Hello World');
+
+console.log(100)
+
+console.log("Hello World")
+
+console.error("Alert")
+
+console.warn("Warning")
+
+
+console.group("Developer Data");
+console.table({name:"Devendra", age:29})
+console.groupEnd();
+
+const style = 'padding: 10px; background-color: white; color:green; font-size:20px'
+console.log('%cHello World', style);
+
+const name = "Devendra";
+
+const arr = [1,2,3,4,5]
+arr.push(6)
+
+console.log(arr)
+
+const a=1, b=2;
+
+const isPresent = true;
+
+const id = Symbol("id")
+
+console.log(id)
+
+
+console.log(1+"1"+"a")
+console.log(1+"1"+"a"+NaN)
+console.log(1+NaN)
+console.log("1"+NaN)
+
